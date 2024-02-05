5fc2d6865174d2db2fb299ae2f1b40aeb557090f
anakhnikian <30841436+anakhnikian@users.noreply.github.com>
2024-02-05 10:53:09 -0500

    Update sdm_to_pow.m

--- spectral_analysis/sdm_to_pow.m
+++ spectral_analysis/sdm_to_pow.m
@@ -1,5 +1,16 @@
 function [psd] = sdm_to_pow(s)
 
+%FUNCTION: Extracts the power spectral densities along the diagonals of the
+%spectral density matrix s
+%
+%INPUT: s is a channels by channels by frequencies array of spectral
+%matrices
+%
+%OUTPUT:psd is a channels by frequencies matrix of one-sided power spectral
+%densities.
+%
+%A. Nakhnikian, 2024
+
 n_chans = size(s,1);
 n_freqs = size(s,3);
 
